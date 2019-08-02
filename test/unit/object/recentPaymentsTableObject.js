a1c569ffca5cef432283bed51e80438649fddc33
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-08-02 12:12:37 +0100

    feat(payment-history): more detailed payment history
    * Unit test
    * Controller
    * View
    
    Jira SA-1757
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/unit/object/recentPaymentsTableObject.js
+++ test/unit/object/recentPaymentsTableObject.js
@@ -2,34 +2,53 @@ const assert = require('assert');
 
 const recentPaymentsTableObject = require('../../../lib/objects/recentPaymentsTableObject');
 
-const sentStatusHtml = '<span class="govuk-!-font-size-16 govuk-!-font-weight-bold gysp-secondary-text-colour gysp-status gysp-status--active">Sent</span>';
-const paidStatusHtml = '<span class="govuk-!-font-size-16 govuk-!-font-weight-bold gysp-secondary-text-colour gysp-status gysp-status--active">Paid</span>';
+const sentStatus = 'Sent';
+const paidStatus = 'Paid';
 
 const recentPayments = {
   recentPayments: [
-    { creditDate: '2019-01-01', paymentAmount: '200.20', status: 'SENT' },
-    { creditDate: '2019-02-01', paymentAmount: '300.20', status: 'PAID' },
-    { creditDate: '2019-03-01', paymentAmount: '400.20', status: 'PAID' },
+    {
+      startDate: '2018-12-01', endDate: '2019-01-01', creditDate: '2019-01-01', paymentAmount: '200.20', status: 'SENT',
+    },
+    {
+      startDate: '2019-01-01', endDate: '2019-02-01', creditDate: '2019-02-01', paymentAmount: '300.20', status: 'PAID',
+    },
+    {
+      startDate: '2019-02-01', endDate: '2019-03-01', creditDate: '2019-03-01', paymentAmount: '400.20', status: 'PAID',
+    },
   ],
 };
 
 const recentPaymentsResponseObject = {
-  caption: 'Recent payments',
+  caption: 'Payment history',
+  head: [
+    { text: 'From' },
+    { text: 'To' },
+    { text: 'Payment date' },
+    { text: 'Amount' },
+    { text: 'Status' },
+  ],
   rows: [
     [
-      { text: '1 January 2019', classes: 'gysp-table__cell--first' },
+      { text: '01/12/2018', classes: 'gysp-table__cell--first' },
+      { text: '01/01/2019', classes: 'gysp-table__cell--first' },
+      { text: '01/01/2019', classes: 'gysp-table__cell--first' },
       { text: '£200.20', classes: 'gysp-table__cell--first' },
-      { html: sentStatusHtml, classes: 'gysp-table__cell--first' },
+      { text: sentStatus, classes: 'gysp-table__cell--first' },
     ],
     [
-      { text: '1 February 2019' },
+      { text: '01/01/2019' },
+      { text: '01/02/2019' },
+      { text: '01/02/2019' },
       { text: '£300.20' },
-      { html: paidStatusHtml },
+      { text: paidStatus },
     ],
     [
-      { text: '1 March 2019' },
+      { text: '01/02/2019' },
+      { text: '01/03/2019' },
+      { text: '01/03/2019' },
       { text: '£400.20' },
-      { html: paidStatusHtml },
+      { text: paidStatus },
     ],
   ],
 };
