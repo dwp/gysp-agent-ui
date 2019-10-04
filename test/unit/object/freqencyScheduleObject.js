5c692ecd752e005d56e5883ea56772e6d689047e
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-10-04 10:58:03 +0100

    feat(payment-status): ability to update status from  paid to not paid
    * Unit tests
    * Controller
    * Objects
    * View
    
    Jira SA-1944
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/unit/object/freqencyScheduleObject.js
+++ test/unit/object/freqencyScheduleObject.js
@@ -2,11 +2,17 @@ const { assert } = require('chai');
 
 const object = require('../../../lib/objects/paymentHistoryDetailViewObject');
 
+const id = 123;
+
 const detail = {
   status: 'SENT',
   accountName: 'Mr R H Smith',
   accountNumber: '98765432',
   sortCode: '400500',
+  totalAmount: 100,
+  startDate: '2019-07-30T00:00:00.000+0000',
+  endDate: '2019-08-27T00:00:00.000+0000',
+  creditDate: '2019-08-27T06:00:00',
 };
 
 const detailFormatted = {
@@ -14,6 +20,21 @@ const detailFormatted = {
   accountHolder: 'Mr R H Smith',
   accountNumber: '98765432',
   sortCode: '40 05 00',
+  detailsSummaryRows: [
+    {
+      key: { classes: 'govuk-!-width-one-third', text: 'payment-detail:summary-keys.total' },
+      value: { classes: 'govuk-!-font-weight-bold', text: '£100.00' },
+    },
+    {
+      key: { classes: 'govuk-!-width-one-third', text: 'payment-detail:summary-keys.period' },
+      value: { html: '30/07/2019 to<br />27/08/2019' },
+    },
+    {
+      key: { classes: 'govuk-!-width-one-third', text: 'payment-detail:summary-keys.status' },
+      value: { text: 'Sent' },
+    },
+  ],
+  id: 123,
 };
 
 describe('payment history detail object formatter', () => {
@@ -21,6 +42,6 @@ describe('payment history detail object formatter', () => {
     assert.isFalse(object.formatter(undefined));
   });
   it('should return valid json when object is called with unformatted object', () => {
-    assert.deepEqual(object.formatter(detail), detailFormatted);
+    assert.deepEqual(object.formatter(detail, id), detailFormatted);
   });
 });
