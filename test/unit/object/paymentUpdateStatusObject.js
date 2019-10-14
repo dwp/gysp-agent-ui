1d7aca419b672ddb2a128310d8d9a2760b3d98db
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-10-14 10:12:26 +0100

    chore(payment-status): returned payment status
    * Unit tests
    * Controller
    * Object
    
    Jira SA-1995
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/unit/object/paymentUpdateStatusObject.js
+++ test/unit/object/paymentUpdateStatusObject.js
@@ -3,30 +3,15 @@ const assert = require('assert');
 const object = require('../../../lib/objects/paymentUpdateStatusObject');
 
 const id = 123;
-const detailsWithPaidStatus = {
-  status: 'PAID',
-};
+const inviteKey = 'TES12345678';
 
-const detailsWithPaidStatusResult = {
+const detailsResult = {
   id,
-  status: 'NOTPAID',
-};
-
-const detailsWithNotPaidStatus = {
-  status: 'NOTPAID',
-};
-
-const detailsWithNotPaidStatusResult = {
-  id,
-  status: 'NOTPAID',
+  inviteKey,
 };
 
 describe('payment status update object formatter', () => {
-  it('should return valid json when object is called with PAID status', () => {
-    assert.deepEqual(object.formatter(detailsWithPaidStatus, id), detailsWithPaidStatusResult);
-  });
-
-  it('should return valid json with current status when object is called with NOTPAID status', () => {
-    assert.deepEqual(object.formatter(detailsWithNotPaidStatus, id), detailsWithNotPaidStatusResult);
+  it('should return valid json when object is called ', () => {
+    assert.deepEqual(object.formatter(id, inviteKey), detailsResult);
   });
 });
