5a6d6dfb9fa4fb805f726d2ad405bcd03f6b5073
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2019-12-19 11:41:20 +0000

    feat(performance): improvements
    Jira SA-2302
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- app/routes/find-someone/functions.js
+++ app/routes/find-someone/functions.js
@@ -1,6 +1,8 @@
 const request = require('request-promise');
 const httpStatus = require('http-status-codes');
 const requestHelper = require('../../../lib/requestHelper');
+const dataStore = require('../../../lib/dataStore');
+const deleteSession = require('../../../lib/deleteSession');
 
 const formValidator = require('../../../lib/formValidator');
 const findSomeoneFormatter = require('../../../lib/findSomeoneDetailsObject');
@@ -32,33 +34,32 @@ function postFindSomeoneErrorHandle(err, req, res) {
 }
 
 function getFindSomeone(req, res) {
-  if (req.session && req.session.searchedNino) {
-    delete req.session.searchedNino;
-  }
   res.render('pages/find-someone/search');
 }
 
-function postFindSomeone(req, res) {
+async function postFindSomeone(req, res) {
   const details = req.body;
   const errors = formValidator.ninoDetails(req.body);
   if (Object.keys(errors).length === 0) {
     const search = bodyOrReturnedStatus(req);
-    const getSearch = requestHelper.generateGetCallWithFullResponse(res.locals.agentGateway + citizenAPIEndpoint + search, {}, 'batch');
-    request(getSearch).then((citizenDetails) => {
-      if (citizenDetails.statusCode === httpStatus.NOT_FOUND || citizenDetails.statusCode === httpStatus.NOT_ACCEPTABLE) {
+    try {
+      const award = await dataStore.cacheRetriveAndStore(req, null, 'awardDetails', () => {
+        const awardCall = requestHelper.generateGetCall(`${res.locals.agentGateway}${citizenAPIEndpoint}${search}`, {}, 'batch');
+        return request(awardCall);
+      });
+      req.session.searchedNino = award.nino;
+      res.render('pages/find-someone/search', { details: { result: findSomeoneFormatter.formatter(award) } });
+    } catch (err) {
+      deleteSession.deleteChangesEnquiries(req);
+      if (err.statusCode === httpStatus.NOT_FOUND) {
         details.noResult = true;
         res.render('pages/find-someone/search', { details });
-      } else if (citizenDetails.statusCode === httpStatus.OK) {
-        req.session.searchedNino = citizenDetails.body.nino;
-        res.render('pages/find-someone/search', { details: { result: findSomeoneFormatter.formatter(citizenDetails.body) } });
       } else {
-        throw citizenDetails;
+        const traceID = requestHelper.getTraceID(err);
+        requestHelper.loggingHelper(err, citizenAPIEndpoint + search, traceID, res.locals.logger);
+        postFindSomeoneErrorHandle(err, req, res);
       }
-    }).catch((err) => {
-      const traceID = requestHelper.getTraceID(err);
-      requestHelper.loggingHelper(err, citizenAPIEndpoint + search, traceID, res.locals.logger);
-      postFindSomeoneErrorHandle(err, req, res);
-    });
+    }
   } else {
     res.render('pages/find-someone/search', { details, errors });
   }
--- gulp-package.js
+++ gulp-package.js
@@ -24,6 +24,7 @@ function build() {
     'lib/**',
     'locales/**',
     'logs',
+    'package.json',
     '*.js',
     '!app/assets/**',
     '!gulpfile.js/**',
