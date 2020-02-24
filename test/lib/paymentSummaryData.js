93216436e72d6d05726eeaaac71fd8e9e5433aeb
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2020-02-24 12:30:59 +0000

    feat(payment): remove last payment
    Jira SA-2093
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/lib/paymentSummaryData.js
+++ test/lib/paymentSummaryData.js
@@ -1,23 +1,25 @@
 module.exports = {
-  validFirstPaymentPaid() {
+  validFirstPaymentNotSent() {
     return JSON.parse(JSON.stringify(this.validBase()));
   },
-  validFirstPaymentNotPaid() {
+  validFirstPaymentSent() {
     const json = JSON.parse(JSON.stringify(this.validBase()));
-    json.firstPaymentPaid = false;
+    json.firstAmount = null;
+    json.firstCreditDate = null;
     return json;
   },
-  validFirstPaymentPaidWithoutNextAmount() {
-    const json = JSON.parse(JSON.stringify(this.validBase()));
-    json.nextAmount = null;
-    json.nextCreditDate = null;
-    return json;
+  validFirstAndNextPaymentNull() {
+    return {
+      firstAmount: null,
+      firstCreditDate: null,
+      nextAmount: null,
+      nextCreditDate: null,
+    };
   },
   validBase() {
     return {
-      firstLastAmount: '203.57',
-      firstLastCreditDate: '2019-04-11T12:49:37.815Z',
-      firstPaymentPaid: true,
+      firstAmount: '203.57',
+      firstCreditDate: '2019-04-11T12:49:37.815Z',
       nextAmount: '101.83',
       nextCreditDate: '2019-04-19T12:49:37.815Z',
     };
