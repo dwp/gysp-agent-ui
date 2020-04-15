d1b05fce6199373c19bd0043544fd3ec8b7b5e8a
Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>
2020-04-15 11:24:35 +0100

    feat(marital): change date
    Jira SA-2447
    
    Signed-off-by: Malcolm Hire <malcolm.hire@engineering.digital.dwp.gov.uk>

--- test/unit/object/api/maritalDetailsObject.js
+++ test/unit/object/api/maritalDetailsObject.js
@@ -93,6 +93,73 @@ const ninoAddFormatted = {
 };
 const ninoChangeFormatted = Object.assign(JSON.parse(JSON.stringify(ninoAddFormatted)), { eventType: 'CHANGE' });
 
+
+const maritalDateBaseFormatted = {
+  nino: 'AA370773A',
+  eventCategory: 'PERSONAL',
+  eventType: 'CHANGE',
+};
+
+const partnerDetail = {
+  firstName: 'Jane',
+  surname: 'Bloogs',
+  allOtherNames: 'Middle',
+  dob: '1952-03-19T00:00:00.000Z',
+};
+
+const maritalDateFormatted = {
+  'married-verified': {
+    ...maritalDateBaseFormatted,
+    eventName: 'personal:timeline.marital-date.married.verified',
+    maritalStatus: 'Married',
+    maritalStatusVerified: true,
+    partnerDetail: {
+      ...partnerDetail,
+      marriageDate: '2020-03-01T00:00:00.000Z',
+    },
+  },
+  'civil-verified': {
+    ...maritalDateBaseFormatted,
+    eventName: 'personal:timeline.marital-date.civil.verified',
+    maritalStatus: 'Civil Partnership',
+    maritalStatusVerified: true,
+    partnerDetail: {
+      ...partnerDetail,
+      civilPartnershipDate: '2020-03-01T00:00:00.000Z',
+    },
+  },
+  'divorced-verified': {
+    ...maritalDateBaseFormatted,
+    eventName: 'personal:timeline.marital-date.divorced.verified',
+    maritalStatus: 'Divorced',
+    maritalStatusVerified: true,
+    partnerDetail: {
+      ...partnerDetail,
+      divorcedDate: '2020-03-01T00:00:00.000Z',
+    },
+  },
+  'dissolved-verified': {
+    ...maritalDateBaseFormatted,
+    eventName: 'personal:timeline.marital-date.dissolved.verified',
+    maritalStatus: 'Dissolved',
+    maritalStatusVerified: true,
+    partnerDetail: {
+      ...partnerDetail,
+      dissolvedDate: '2020-03-01T00:00:00.000Z',
+    },
+  },
+  'widowed-verified': {
+    ...maritalDateBaseFormatted,
+    eventName: 'personal:timeline.marital-date.widowed.verified',
+    maritalStatus: 'Widowed',
+    maritalStatusVerified: true,
+    partnerDetail: {
+      ...partnerDetail,
+      widowedDate: '2020-03-01T00:00:00.000Z',
+    },
+  },
+};
+
 const dateDetailsVerified = {
   dateYear: '2020', dateMonth: '3', dateDay: '1', verification: 'V',
 };
@@ -104,32 +171,51 @@ const partnerNinoDetails = { partnerNino: 'AA123456A' };
 
 describe('maritalDetailsObject', () => {
   describe('formatter', () => {
-    describe('Current marital status - Married', () => {
-      it('should return formatted update divorced marital details object when verified divorced date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'divorced', claimData.validClaimMarried()), marriedToDivorcedVerifiedFormatted);
+    describe('marital status', () => {
+      describe('Current marital status - Married', () => {
+        it('should return formatted update divorced marital details object when verified divorced date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'divorced', claimData.validClaimMarried()), marriedToDivorcedVerifiedFormatted);
+        });
+        it('should return formatted update divorced marital details object when non verified divorced date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'divorced', claimData.validClaimMarried()), marriedToDivorcedNonVerifiedFormatted);
+        });
+        it('should return formatted update widowed marital details object when verified widowed date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'widowed', claimData.validClaimMarried()), marriedToWidowedVerifiedFormatted);
+        });
+        it('should return formatted update widowed marital details object when non verified widowed date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'widowed', claimData.validClaimMarried()), marriedToWidowedNonVerifiedFormatted);
+        });
       });
-      it('should return formatted update divorced marital details object when non verified divorced date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'divorced', claimData.validClaimMarried()), marriedToDivorcedNonVerifiedFormatted);
-      });
-      it('should return formatted update widowed marital details object when verified widowed date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'widowed', claimData.validClaimMarried()), marriedToWidowedVerifiedFormatted);
-      });
-      it('should return formatted update widowed marital details object when non verified widowed date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'widowed', claimData.validClaimMarried()), marriedToWidowedNonVerifiedFormatted);
+      describe('Current marital status - Civil Partnership', () => {
+        it('should return formatted update dissolved marital details object when verified dissolved date supplied ', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'dissolved', claimData.validClaimCivilPartner()), civilToDissolvedVerifiedFormatted);
+        });
+        it('should return formatted update dissolved marital details object when non verified dissolved date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'dissolved', claimData.validClaimCivilPartner()), civilToDissolvedNonVerifiedFormatted);
+        });
+        it('should return formatted update widowed marital details object when verified widowed date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'widowed', claimData.validClaimCivilPartner()), civilToWidowedVerifiedFormatted);
+        });
+        it('should return formatted update widowed marital details object when non verified widowed date supplied', () => {
+          assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'widowed', claimData.validClaimCivilPartner()), civilToWidowedNonVerifiedFormatted);
+        });
       });
     });
-    describe('Current marital status - Civil Partnership', () => {
-      it('should return formatted update dissolved marital details object when verified dissolved date supplied ', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'dissolved', claimData.validClaimCivilPartner()), civilToDissolvedVerifiedFormatted);
+    describe('marital date', () => {
+      it('should return formatted married marital details object when status has not changed', () => {
+        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'married', claimData.validClaimMarried()), maritalDateFormatted['married-verified']);
       });
-      it('should return formatted update dissolved marital details object when non verified dissolved date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'dissolved', claimData.validClaimCivilPartner()), civilToDissolvedNonVerifiedFormatted);
+      it('should return formatted civil partner marital details object when status has not changed', () => {
+        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'civil', claimData.validClaimCivilPartner()), maritalDateFormatted['civil-verified']);
       });
-      it('should return formatted update widowed marital details object when verified widowed date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'widowed', claimData.validClaimCivilPartner()), civilToWidowedVerifiedFormatted);
+      it('should return formatted divorced marital details object when status has not changed', () => {
+        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'divorced', claimData.validClaimDivorced()), maritalDateFormatted['divorced-verified']);
       });
-      it('should return formatted update widowed marital details object when non verified widowed date supplied', () => {
-        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsNotVerified, 'widowed', claimData.validClaimCivilPartner()), civilToWidowedNonVerifiedFormatted);
+      it('should return formatted dissolved marital details object when status has not changed', () => {
+        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'dissolved', claimData.validClaimDissolved()), maritalDateFormatted['dissolved-verified']);
+      });
+      it('should return formatted widowed marital details object when status has not changed', () => {
+        assert.deepEqual(maritalDetailsObject.formatter(dateDetailsVerified, 'widowed', claimData.validClaimWidowed()), maritalDateFormatted['widowed-verified']);
       });
     });
   });
